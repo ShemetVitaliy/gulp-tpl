59aa830d7ad8e9898f2ddfd89f9192b6cca2a8d6
Vitaliy Shemet <shemetvm@megabank.ua>
2022-05-14 14:01:25 +0300

    settings gulp

--- src/js/modules/send-form.js
+++ src/js/modules/send-form.js
@@ -1,18 +1,15 @@
-export function sendForm() {
-  console.log('sendForm');
-  $('.send-form').submit(function() {
-    const th = $(this);
-    $.ajax({
-      type: 'POST',
-      url: '../mail.php',
-      data: th.serialize()
-    }).done(function() {
-      $('.thanks-message').addClass('open');
-      setTimeout(function() {
-        $('.thanks-message').removeClass('open');
-        th.trigger('reset');
-      }, 3000);
-    });
-    return false;
+$('.send-form').submit(function() {
+  const th = $(this);
+  $.ajax({
+    type: 'POST',
+    url: '../mail.php',
+    data: th.serialize()
+  }).done(function() {
+    $('.thanks-message').addClass('open');
+    setTimeout(function() {
+      $('.thanks-message').removeClass('open');
+      th.trigger('reset');
+    }, 3000);
   });
-}
+  return false;
+});
